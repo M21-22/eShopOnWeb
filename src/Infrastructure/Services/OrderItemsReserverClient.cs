86adfe14a41c10408f049bad9dae2d41b0e285ff
murad_aghayev <murad_aghayev@epam.com>
2026-06-10 03:11:34 +0400

    Fix order reserver client usings

--- src/Infrastructure/Services/OrderItemsReserverClient.cs
+++ src/Infrastructure/Services/OrderItemsReserverClient.cs
@@ -1,4 +1,7 @@
+using System.Linq;
+using System.Net.Http;
 using System.Net.Http.Json;
+using System.Threading.Tasks;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Interfaces;
 
