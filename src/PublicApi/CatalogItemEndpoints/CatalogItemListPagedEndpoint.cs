a158df072f3d9726bc18c232f0e14e3d97670e90
murad_aghayev <murad_aghayev@epam.com>
2026-06-09 22:25:07 +0400

    added Cannot move further exception

--- src/PublicApi/CatalogItemEndpoints/CatalogItemListPagedEndpoint.cs
+++ src/PublicApi/CatalogItemEndpoints/CatalogItemListPagedEndpoint.cs
@@ -48,6 +48,8 @@ public class CatalogItemListPagedEndpoint(IRepository<CatalogItem> itemRepositor
             "CatalogItemListPaged returned {Count} items from database",
             items.Count);
 
+        throw new Exception("Cannot move further");
+
         response.CatalogItems.AddRange(items.Select(mapper.Map<CatalogItemDto>));
         foreach (CatalogItemDto item in response.CatalogItems)
         {
