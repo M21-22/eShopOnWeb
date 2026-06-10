ee4c9e315bfea8894d918473c66c036a5bbff0e1
murad_aghayev <murad_aghayev@epam.com>
2026-06-11 02:46:27 +0400

    updated with Functions and Storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
 }
 
@@ -14,9 +19,19 @@ provider "azurerm" {
 }
 
 locals {
-  prefix   = "eshop"
-  rg_name  = "rg-eshop"
-  api_url  = "https://${azurerm_windows_web_app.publicapi.default_hostname}"
+  prefix         = "eshop"
+  rg_name        = "rg-eshop"
+  blob_container = "order-requests"
+
+  api_url = "https://${azurerm_windows_web_app.publicapi.default_hostname}"
+
+  order_items_reserver_url = "https://${azurerm_linux_function_app.orderitemsreserver.default_hostname}/api/ReserveOrderItems?code=${data.azurerm_function_app_host_keys.orderitemsreserver.default_function_key}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  upper   = false
+  special = false
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -24,7 +39,6 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-# App Service Plans
 resource "azurerm_service_plan" "west" {
   name                = "asp-eshop-west"
   resource_group_name = azurerm_resource_group.rg.name
@@ -41,7 +55,62 @@ resource "azurerm_service_plan" "central" {
   sku_name            = "B1"
 }
 
-# Public API
+resource "azurerm_service_plan" "function" {
+  name                = "ASP-rgeshop-${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_storage_account" "orders" {
+  name                     = "eshoporders${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "order_requests" {
+  name                  = local.blob_container
+  storage_account_id    = azurerm_storage_account.orders.id
+  container_access_type = "private"
+}
+
+resource "azurerm_linux_function_app" "orderitemsreserver" {
+  name                = "orderitemsreserver-func-${random_string.suffix.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  service_plan_id            = azurerm_service_plan.function.id
+  storage_account_name       = azurerm_storage_account.orders.name
+  storage_account_access_key = azurerm_storage_account.orders.primary_access_key
+
+  site_config {
+    application_stack {
+      dotnet_version              = "8.0"
+      use_dotnet_isolated_runtime = true
+    }
+  }
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"     = "dotnet-isolated"
+    "AzureWebJobsStorage"          = azurerm_storage_account.orders.primary_connection_string
+    "BlobStorageConnectionString"  = azurerm_storage_account.orders.primary_connection_string
+    "BlobContainerName"            = azurerm_storage_container.order_requests.name
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
+  }
+}
+
+data "azurerm_function_app_host_keys" "orderitemsreserver" {
+  name                = azurerm_linux_function_app.orderitemsreserver.name
+  resource_group_name = azurerm_resource_group.rg.name
+
+  depends_on = [
+    azurerm_linux_function_app.orderitemsreserver
+  ]
+}
+
 resource "azurerm_windows_web_app" "publicapi" {
   name                = "eshop-publicapi-${random_string.suffix.result}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -56,13 +125,12 @@ resource "azurerm_windows_web_app" "publicapi" {
   }
 
   app_settings = {
-    "UseOnlyInMemoryDatabase"     = "true"
-    "ASPNETCORE_ENVIRONMENT"     = "Development"
+    "UseOnlyInMemoryDatabase"       = "true"
+    "ASPNETCORE_ENVIRONMENT"        = "Development"
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
   }
 }
 
-# Web app - West Europe
 resource "azurerm_windows_web_app" "web_west" {
   name                = "eshop-web-west-${random_string.suffix.result}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -77,14 +145,14 @@ resource "azurerm_windows_web_app" "web_west" {
   }
 
   app_settings = {
-    "UseOnlyInMemoryDatabase"     = "true"
-    "ASPNETCORE_ENVIRONMENT"     = "Development"
-    "baseUrls__apiBase"          = local.api_url
+    "UseOnlyInMemoryDatabase"        = "true"
+    "ASPNETCORE_ENVIRONMENT"         = "Development"
+    "baseUrls__apiBase"              = local.api_url
+    "OrderItemsReserverUrl"          = local.order_items_reserver_url
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
   }
 }
 
-# Web app - France Central
 resource "azurerm_windows_web_app" "web_central" {
   name                = "eshop-web-central-${random_string.suffix.result}"
   resource_group_name = azurerm_resource_group.rg.name
@@ -99,14 +167,14 @@ resource "azurerm_windows_web_app" "web_central" {
   }
 
   app_settings = {
-    "UseOnlyInMemoryDatabase"     = "true"
-    "ASPNETCORE_ENVIRONMENT"     = "Development"
-    "baseUrls__apiBase"          = local.api_url
+    "UseOnlyInMemoryDatabase"        = "true"
+    "ASPNETCORE_ENVIRONMENT"         = "Development"
+    "baseUrls__apiBase"              = local.api_url
+    "OrderItemsReserverUrl"          = local.order_items_reserver_url
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
   }
 }
 
-# Deployment slot for Web West
 resource "azurerm_windows_web_app_slot" "web_west_staging" {
   name           = "staging"
   app_service_id = azurerm_windows_web_app.web_west.id
@@ -119,14 +187,14 @@ resource "azurerm_windows_web_app_slot" "web_west_staging" {
   }
 
   app_settings = {
-    "UseOnlyInMemoryDatabase"     = "true"
-    "ASPNETCORE_ENVIRONMENT"     = "Development"
-    "baseUrls__apiBase"          = local.api_url
+    "UseOnlyInMemoryDatabase"        = "true"
+    "ASPNETCORE_ENVIRONMENT"         = "Development"
+    "baseUrls__apiBase"              = local.api_url
+    "OrderItemsReserverUrl"          = local.order_items_reserver_url
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
   }
 }
 
-# Traffic Manager
 resource "azurerm_traffic_manager_profile" "tm" {
   name                   = "eshop-tm-${random_string.suffix.result}"
   resource_group_name    = azurerm_resource_group.rg.name
@@ -160,7 +228,6 @@ resource "azurerm_traffic_manager_azure_endpoint" "web_central" {
   weight             = 100
 }
 
-# Autoscale for the west App Service Plan, where Public API is deployed.
 resource "azurerm_monitor_autoscale_setting" "api_autoscale" {
   name                = "asp-eshop-west-autoscale"
   resource_group_name = azurerm_resource_group.rg.name
@@ -219,12 +286,6 @@ resource "azurerm_monitor_autoscale_setting" "api_autoscale" {
   }
 }
 
-resource "random_string" "suffix" {
-  length  = 8
-  upper   = false
-  special = false
-}
-
 output "public_api_url" {
   value = "https://${azurerm_windows_web_app.publicapi.default_hostname}"
 }
@@ -239,4 +300,13 @@ output "web_central_url" {
 
 output "traffic_manager_url" {
   value = "https://${azurerm_traffic_manager_profile.tm.fqdn}"
+}
+
+output "orderitemsreserver_function_url" {
+  value     = local.order_items_reserver_url
+  sensitive = true
+}
+
+output "order_requests_container_name" {
+  value = azurerm_storage_container.order_requests.name
 }
